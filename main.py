e2a8fef30169f8e9833d9bb1b54499ffabead32e
lispaupau <152isu@mail.ru>
2024-03-06 18:28:16 +0200

    Use env variables and create Procfile.

--- main.py
+++ main.py
@@ -12,8 +12,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -43,7 +48,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI', 'sqlite:///blog.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -252,4 +257,4 @@ def comment_delete(comment_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
